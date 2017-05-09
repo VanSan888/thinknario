d617c100e245f419f33d20501a9e9e4dd0bf3911
VanSan888 <m.fieser@tu-braunschweig.de>
2017-05-09 15:53:12 +0200

    über umwege quasi merge mit authentification

--- src/pages/einstellungen/einstellungen.ts
+++ src/pages/einstellungen/einstellungen.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
+import { AuthData } from '../../providers/auth/auth';
+
 
 @IonicPage()
 @Component({
@@ -8,9 +10,19 @@ import { IonicPage } from 'ionic-angular';
   templateUrl: 'einstellungen.html'
 })
 export class EinstellungenPage {
+	
+resetPasswordPage = 'ResetPasswordPage'
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public authData: AuthData) {
 
   }
+  
+  logoutUser(): void {
+	
+	this.authData.logoutUser()
+    .then( () => {
+        this.navCtrl.setRoot('LogInPage');
+      });
+  }	
 
 }
---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable()
+export class AuthData {
+
+  constructor() {
+    
+  }
+
+loginUser(email: string, password: string): firebase.Promise<any> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+}
+
+signupUser(email: string, password: string): firebase.Promise<any> {
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then( newUser => {
+        firebase.database().ref('/userProfile').child(newUser.uid)
+        .set({ email: email });
+  });
+}
+
+resetPassword(email: string): firebase.Promise<void> {
+  return firebase.auth().sendPasswordResetEmail(email);
+}
+
+logoutUser(): firebase.Promise<void> {
+  return firebase.auth().signOut();
+}
+  
+}
